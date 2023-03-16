50d85e3527e59425540b7c62f57b7fa4ff820b34
risaratri <karisaarfinaratri@gmail.com>
2023-03-16 08:23:10 +0700

    Menambahkan file Exercise-01

---
+++ Exercise-01.py
@@ -0,0 +1,8 @@
+print("Nama : Karisa Arfina Ratri")
+print("NIM  : I0322061")
+print("Asal : Kab. Sukoharjo")
+print("Kabupaten Sukoharjo merupakan salah satu kabuaten yang terletak di Provinsi Jawa Tengah dan berbatasan langsung dengan Kota Surakarta.")
+print("Kabupaten Sukoharjo memiliki berbagai julukan, salah satunya adalah kota jamu. Hal tersebut dikarenakan di Kabupaten Sukoharjo terdapat sentra Jamu yang terletak pada Kecamatan Nguter.")
+print("Kabupaten Sukoharjo juga terkenal dengan julukan kota tekstil. Hal tersebut dikarenakan di Kabupaten Sukoharjo ini terdapat berbagai sentra industri tekstil yang besar.")
+print("Selain julukan-julukan tersebut, Kabupaten Sukoharjo juga terkenal karena terdapat sentra industri arak atau disebut dengan ciu. Industri tersebut akan banyak ditemukan di Desa Bekonang, Kecamatan Mojolaban.")
+print("Harapan saya setelah lulus dari jurusan Teknik Industri Universitas Sebelas Maret ini adalah saya dapat menerapkan ilmu-ilmu yang telah saya dapatkan dan dapat berguna untuk orang-orang di sekitar saya.")
